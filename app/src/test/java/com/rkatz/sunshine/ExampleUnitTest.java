edb177ae8e217c4304f8b0f68c52b4a055ea4096
Russell Katz <rkatz1997@hotmail.com>
2016-06-08 22:37:52 -0400

    test commit 2

--- app/src/test/java/com/rkatz/sunshine/ExampleUnitTest.java
+++ app/src/test/java/com/rkatz/sunshine/ExampleUnitTest.java
@@ -12,5 +12,6 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
         assertEquals(4, 2 + 2);
+        assertEquals(4, 2 + 2);
     }
 }
